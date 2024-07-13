23bd7da958a0799a45501a458e1406bc249f9bb2
Hardik Sharma <sharmahardik606@gmail.com>
2024-07-14 00:12:45 +0530

    Random Quote generator

---
+++ script.js
@@ -0,0 +1,24 @@
+const url = "https://api.quotable.io/random";
+const quote = document.querySelector(".p");
+const author = document.querySelector(".p1");
+const newQuote = document.querySelector("#b1");
+const tweet = document.querySelector("#b2");
+
+
+const getQuote = async()=>{
+    let response = await fetch(url);
+    let data = await response.json();
+    quote.innerHTML = data.content;
+    author.innerHTML = "-" + data.author;
+}
+
+
+getQuote()
+newQuote.addEventListener("click",getQuote);
+
+
+function twitter(){
+    window.open("https://twitter.com/intent/tweet?text=Hello%20world");
+}
+
+tweet.addEventListener("click",twitter);
